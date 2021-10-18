a90c0289dab3dbee0de8d5d8d728956d1eab10a2
Luca Borrione <luca@email.com>
2021-10-19 00:02:30 +0100

    Step 8

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,7 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-
-console.log(checkForName);
-
-alert('Hello');
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
